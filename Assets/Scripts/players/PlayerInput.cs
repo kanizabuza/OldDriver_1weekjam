36092f9977e2a7cf679473685ad47ae9a005b8a5
hika04157726@gmail.com <hika.04157726@gmail.com>
2020-02-26 00:36:26 +0900

    add: PlayerInput&PlayerMover

---
+++ Assets/Scripts/players/PlayerInput.cs
@@ -0,0 +1,24 @@
+﻿using UniRx;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    private readonly Subject<Unit> moveRightStream = new Subject<Unit>();
+    private readonly Subject<Unit> moveLeftStream = new Subject<Unit>();
+    private readonly Subject<Unit> skillStream = new Subject<Unit>();
+    public Subject<Unit> MoveRightStream => moveRightStream;
+    public Subject<Unit> MoveLeftStream => moveLeftStream;
+    public Subject<Unit> SkillStream => skillStream;
+
+    /// <summary>
+    /// InputをObserveする
+    /// </summary>
+    private void Start() {
+        InputAsObservable.GetKeyDown(KeyCode.RightArrow)
+            .Subscribe(moveRightStream.OnNext).AddTo(this);
+        InputAsObservable.GetKeyDown(KeyCode.LeftArrow)
+            .Subscribe(moveLeftStream.OnNext).AddTo(this);
+        InputAsObservable.GetKeyDown(KeyCode.Space)
+            .Subscribe(skillStream.OnNext).AddTo(this);
+    }
+}
