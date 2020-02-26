84a8adf1ffbfed1c62f70fdcd334d33f4bb3d75f
hika04157726@gmail.com <hika.04157726@gmail.com>
2020-02-27 01:19:22 +0900

    add: スキルカットイン＆スキルゲージ

--- Assets/Scripts/players/PlayerMover.cs
+++ Assets/Scripts/players/PlayerMover.cs
@@ -19,11 +19,12 @@ public class PlayerMover : MonoBehaviour
                 direction.x = 1.3f * -1;
                 Move(direction);
             }).AddTo(gameObject);
-
-        input.SkillStream
-            .Subscribe(_ => Debug.Log("Skill")).AddTo(gameObject);
     }
 
+    /// <summary>
+    /// 左右移動
+    /// </summary>
+    /// <param name="direction">移動方向</param>
     private void Move(Vector2 direction) {
         Vector2 pos = transform.position;
         if (pos.x <= -1.9f && direction.x == -1.3f) return;
---
+++ Assets/Scripts/players/PlayerSkillExecutor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerSkillExecutor : MonoBehaviour
+{
+    [SerializeField] private Animator cutInAnim;
+    [SerializeField] private GameObject cutIn;
+    [SerializeField] private Slider slider;
+
+    private PlayerInput input;
+    private float skillGauge = 0;
+
+    private void Start() {
+        input = GetComponent<PlayerInput>();
+        input.SkillStream
+            .Where(_ => skillGauge > 90)
+            .Subscribe(__ => ExecuteSkill()).AddTo(gameObject);
+
+        this.ObserveEveryValueChanged(x => x.skillGauge)
+            .Subscribe(_ => slider.value = skillGauge);
+    }
+
+    private void Update() {
+        //test
+        ChargeSkill(1f);
+    }
+
+    /// <summary>
+    /// スキルを実行
+    /// </summary>
+    private void ExecuteSkill() {
+        skillGauge = 0;
+
+        cutIn.gameObject.SetActive(true);
+        cutInAnim.Play("cutInAnimation",0,0.0f);
+        Observable.Timer(TimeSpan.FromSeconds(0.75))
+            .Subscribe(_ => {
+                cutIn.SetActive(false);
+            }).AddTo(this);
+    }
+
+    /// <summary>
+    /// value分だけスキルゲージを溜める
+    /// </summary>
+    /// <param name="value">足す値</param>
+    private void ChargeSkill(float value ) {
+        skillGauge += value;
+        if (skillGauge >= 100) skillGauge = 100;
+    }
+}
